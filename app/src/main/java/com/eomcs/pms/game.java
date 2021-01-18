d646631372f9615b822545bf7270024eed1295f0
CHOISEOKRYEOL <zlshsktl@naver.com>
2021-01-18 23:56:42 +0900

    .

--- app/src/main/java/com/eomcs/pms/game.java
+++ app/src/main/java/com/eomcs/pms/game.java
@@ -4,112 +4,101 @@ import java.util.Scanner;
 
 public class game {
 
+  static Scanner scanner= new Scanner(System.in);
+
+  static final int LENGTH = 4;
+  static int i = 0;
+  static int size = 0;
+
+
+  static String hair[] = new String[LENGTH];
+  static String sex[] = new String[LENGTH];
+  static String ace[] = new String[LENGTH];
+  static int physical[] = new int[LENGTH];
+  static int kind[] = new int[LENGTH];
+  static String warrior[]= new String[LENGTH];
+  static String magician[] = new String[LENGTH];
+  static String archer[] =  new String[LENGTH];
+  static String thief[] = new String[LENGTH];
+
+  static void setcharacter() {
+    System.out.println("1.전사닉네임");
+    warrior[i] = scanner.nextLine();
+    System.out.println("2.마법사닉네임");
+    magician[i] = scanner.nextLine();
+    System.out.println("3.궁수닉네임");
+    archer[i] = scanner.nextLine();
+    System.out.println("4.도적닉네임");
+    thief[i] = scanner.nextLine();
+  }
+
+  static void viewstat() {
+    System.out.println("warrior, magician, archer, thief");
+    String str = scanner.nextLine();
+    if(str.equalsIgnoreCase("warrior")) {
+      System.out.println("힘: 10, 민첩: 4, 마나: 1, 공격속도: 5");
+    }    
+    else if(str.equalsIgnoreCase("magician")) {
+      System.out.println("힘: 3, 민첩: 6, 마나: 10, 공격속도: 3");
+    }
+    else if(str.equalsIgnoreCase("archer")) {
+      System.out.println("힘: 6, 민첩: 6, 마나: 4, 공격속도: 8");
+    }
+    else if(str.equalsIgnoreCase("thief"))
+      System.out.println("힘: 5, 민첩: 9, 마나: 5, 공격속도: 6");
+  }
+
+  static void viewphysical(){
+    System.out.println("소인, 중인, 대인");
+    String str1 = scanner.nextLine();
+    if(str1.equalsIgnoreCase("소인")) {
+      System.out.println("소인");
+    }    
+    else if(str1.equalsIgnoreCase("중인")) {
+      System.out.println("중인");
+    }
+    else if(str1.equalsIgnoreCase("대인")) {
+      System.out.println("대인");
+    }
+  }
+
   public static void main(String[] args) {
 
-    Scanner scanner= new Scanner(System.in);
-
-    final int LENGTH = 4;
-    int i = 0;
-    int size = 0;
-
-
-    String hair[] = new String[LENGTH];
-    String sex[] = new String[LENGTH];
-    String ace[] = new String[LENGTH];
-    int physical[] = new int[LENGTH];
-    int kind[] = new int[LENGTH];
-    String warrior[]= new String[LENGTH];
-    String magician[] = new String[LENGTH];
-    String archer[] =  new String[LENGTH];
-    String thief[] = new String[LENGTH];
-
-
     while(true) {
-
-      System.out.print("직업");
+      System.out.print("job , stat, pyhsical");
       System.out.print(">");
       String commend = scanner.nextLine();
 
       switch(commend) {
-
-        case "job":
-          for(i = 0; i < LENGTH; i++) {
-            System.out.println("1.전사닉네임");
-            warrior[i] = scanner.nextLine();
-            System.out.println("2.마법사닉네임");
-            magician[i] = scanner.nextLine();
-            System.out.println("3.궁수닉네임");
-            archer[i] = scanner.nextLine();
-            System.out.println("4.도적닉네임");
-            thief[i] = scanner.nextLine();
-            System.out.print(">");;
-            System.out.println("1. 전사 2. 마법사 3. 궁수 4. 도적 능력치" );
-            kind[i] = scanner.nextInt();
-            if(kind[i] > 4) {
-              System.out.println("선택하신 캐릭터의 능력치가 없습니다.");
-              break;
-            }
-
-
-            switch (kind[i]) {
-              case 1 :
-                System.out.println("힘: 10, 민첩: 4, 마나: 1, 공격속도: 5");
-                break;
-              case 2 :
-                System.out.println("힘: 3, 민첩: 6, 마나: 10, 공격속도: 3");
-                break;
-              case 3 :
-                System.out.println("힘: 6, 민첩: 6, 마나: 4, 공격속도: 8");
-                break;
-              case 4 :
-                System.out.println("힘: 5, 민첩: 9, 마나: 5, 공격속도: 6");
-                break;
-            }
-            System.out.println();
-            scanner.nextLine();
-            System.out.print("헤어스타일 >");
-            hair[i]= scanner.nextLine();
-
-            System.out.print("성별 > (m/w) ");
-            sex[i] = scanner.nextLine();
-
-
-            System.out.print("무기 >");
-            ace[i] = scanner.nextLine();
-
-            System.out.println("체형: 1.소인 2. 중인 3. 대인");
-            System.out.print(">");
-            physical[i] = scanner.nextInt();
-            scanner.nextLine();
-            switch(physical[i]) {
-              case 1 :
-                System.out.println("소인");
-                break;
-              case 2 :
-                System.out.println("중인");
-                break;
-              case 3 :
-                System.out.println("대인");
-                break;
-              default : 
-                System.out.println("선택된 체형은 없습니다.");
-                break;
-            }
-            size++;
-
-            System.out.println();
-
-            System.out.print("계속 입력하시겠습니까?(y/N) ");
-            String str = scanner.nextLine();
-            if (!str.equalsIgnoreCase("y")) {
-              break;
-            }
-            System.out.println();
-          }
+        case "job": 
+          setcharacter();
+          break;
+        case "stat" :
+          viewstat();
+          break;
+        case "physical" :
+          viewphysical();
       }
+
+      System.out.println(); 
+      System.out.print("헤어스타일 >");
+      hair[i]= scanner.nextLine();
+
+      System.out.print("성별 > (m/w) ");
+      sex[i] = scanner.nextLine();
+
+
+      System.out.print("무기 >");
+      ace[i] = scanner.nextLine();
+
+      size++;
+
+      System.out.println();
+
       for(i =0; i < size; i++) {
-        System.out.printf(" %d, %s, %s, %s, %d\n", kind[i],hair[i],sex[i],ace[i],physical[i]);
+        System.out.printf(" %s, %s, %s, %s, %s, %s, %s, %s, %s\n",warrior[i],magician[i],archer[i],thief[i],kind[i],hair[i],sex[i],ace[i],physical[i]);
       }
     }
   }
 }
+
