21a7b2a3ea553bc88746eedbf79fb81feb4b8630
CHOISEOKRYEOL <zlshsktl@naver.com>
2021-01-19 17:39:23 +0900

    .

--- app/src/main/java/com/eomcs/pms/game.java
+++ app/src/main/java/com/eomcs/pms/game.java
@@ -15,7 +15,6 @@ public class game {
   static String sex[] = new String[LENGTH];
   static String ace[] = new String[LENGTH];
   static int physical[] = new int[LENGTH];
-  static int kind[] = new int[LENGTH];
   static String warrior[]= new String[LENGTH];
   static String magician[] = new String[LENGTH];
   static String archer[] =  new String[LENGTH];
@@ -24,16 +23,38 @@ public class game {
   static String str1[] = new String[LENGTH];
 
 
-  static void setcharacter() {
+  static void setjob() {
     System.out.println("1.전사닉네임");
     warrior[i] = scanner.nextLine();
+
     System.out.println("2.마법사닉네임");
     magician[i] = scanner.nextLine();
+
     System.out.println("3.궁수닉네임");
     archer[i] = scanner.nextLine();
+
     System.out.println("4.도적닉네임");
     thief[i] = scanner.nextLine();
+
+    System.out.println(); 
+    System.out.print("헤어스타일 >");
+    hair[i]= scanner.nextLine();
+
+    System.out.print("성별 > (m/w) ");
+    sex[i] = scanner.nextLine();
+
+    System.out.print("무기 >");
+    ace[i] = scanner.nextLine();
   }
+
+  static void viewjob() {
+    for(i =0; i < size; i++) {
+      System.out.printf(" %s, %s, %s, %s, %s, %s, %s ",
+          warrior[i],magician[i],archer[i],thief[i],hair[i],sex[i],ace[i]);
+      size++;
+    }
+  }
+
 
   static void viewstat() {
     System.out.println("warrior, magician, archer, thief");
@@ -67,41 +88,32 @@ public class game {
 
   public static void main(String[] args) {
 
-    while(true) {
-      System.out.print("job , stat, pyhsical");
-      System.out.print(">");
-      String commend = scanner.nextLine();
+    loop:
+      while(true) {
+        System.out.print("명령");
+        System.out.print(">");
+        String commend = scanner.nextLine();
 
-      switch(commend) {
-        case "job": 
-          setcharacter();
-          break;
-        case "stat" :
-          viewstat();
-          break;
-        case "physical" :
-          viewphysical();
+        switch(commend) {
+          case "setjob": 
+            setjob();
+            break;
+          case "viewjob" :
+            viewjob();
+            break;
+          case "stat" :
+            viewstat();
+            break;
+          case "physical" :
+            viewphysical();
+            break;
+          case "exit" :
+            System.out.println("Bye");
+            break loop;
+        }
+
       }
-
-      System.out.println(); 
-      System.out.print("헤어스타일 >");
-      hair[i]= scanner.nextLine();
-
-      System.out.print("성별 > (m/w) ");
-      sex[i] = scanner.nextLine();
-
-
-      System.out.print("무기 >");
-      ace[i] = scanner.nextLine();
-
-      size++;
-
-      System.out.println();
-
-      for(i =0; i < size; i++) {
-        System.out.printf(" %s, %s, %s, %s, %s, %s, %s, %s, %s\n",warrior[i],magician[i],archer[i],thief[i],kind[i],hair[i],sex[i],ace[i],physical[i]);
-      }
-    }
+    scanner.close();
   }
 }
 
