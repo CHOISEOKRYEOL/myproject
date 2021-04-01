b4521e1a38ffceae4e5bd30053176b9b04db107e
CHOISEOKRYEOL <zlshsktl@naver.com>
2021-04-01 22:00:23 +0900

    .

--- app/src/main/java/com/eomcs/mybatis/mybatis.java
+++ app/src/main/java/com/eomcs/mybatis/mybatis.java
@@ -1,23 +1,15 @@
 package com.eomcs.mybatis;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class mybatis {
 
   public static void main(String[] args) throws Exception {
 
-    InputStream mybatisConfigInputStream = new FileInputStream(
-        "./bin/main/com/eomcs/mybatis/mybatis-config.xml");
-
-    SqlSessionFactoryBuilder factoryBuilder = new SqlSessionFactoryBuilder();
-
-    SqlSessionFactory factory = factoryBuilder.build(mybatisConfigInputStream);
-
-    SqlSession sqlSession = factory.openSession();
+    SqlSession sqlSession = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(
+        "com/eomcs/mybatis/mybatis-config.xml")).openSession();
 
     System.out.println("mybatis 사용 준비 완료!");
 
